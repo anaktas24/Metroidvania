e51f60c5039a7a104a5f69f0da6485e472b3a068
Ioannis <72883899+anaktas24@users.noreply.github.com>
2024-04-09 20:51:59 +0200

    dash

--- Scenes/Player/STATES.gd
+++ Scenes/Player/STATES.gd
@@ -1,5 +1,13 @@
 extends Node
 
+@onready var IDLE = $IDLE
+@onready var MOVE = $MOVE
+@onready var JUMP = $JUMP
+@onready var FALL = $FALL
+@onready var DASH = $DASH
+@onready var SLIDE = $SLIDE
+
+#@onready var CLIMB = $CLIMB
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
